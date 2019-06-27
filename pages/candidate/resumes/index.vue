44e2482635d0306e46e139315169a2f13f377c74
jeanquark <jm.kleger@gmail.com>
2019-06-27 18:37:10 +0200

    Added Google chart for click statistics

--- pages/candidate/resumes/index.vue
+++ pages/candidate/resumes/index.vue
@@ -2,6 +2,7 @@
     <v-layout row wrap>
     	<!-- <b>loadedUser:</b> {{ loadedUser }}<br /> -->
     	<!-- <b>loadedUserResumes:</b> {{ loadedUserResumes }}<br /> -->
+		<!-- getStatistics: {{ getStatistics }}<br /><br /> -->
         <v-layout justify-center>
             <h2>My resumes</h2><br /><br />
         </v-layout>
@@ -55,9 +56,32 @@
 			</v-card>
             <!-- <edit-resume-component :candidate-id="loadedUser.id"></edit-resume-component> -->
         </v-flex>
-        <v-flex xs10 offset-xs1 class="text-xs-center">
+
+
+
+        <v-flex xs10 offset-xs1 class="text-xs-center my-5">
             <!-- <create-resume-component></create-resume-component> -->
+			<h2>Statistics</h2>
+			<small>(# clicks on resumes)</small><br /><br />
+			<GChart
+			 	type="ColumnChart"
+			 	:data="chartData"
+			 	:options="chartOptions"
+				class="ma-2"
+			 	@ready="onChartReady"
+			/>
+			<br /><br />
+			Total number of clicks:<br /><br />
+			<div v-for="resume in loadedUserResumes" :key="resume.id">
+				<p v-if="resume.statistics_last_visits">
+					resume "{{ resume.slug }}":
+					{{ resume.statistics_last_visits.length }}
+				</p>
+			</div>
         </v-flex>
+
+
+
 
         <v-snackbar
 	      	v-model="snackbar"
@@ -89,7 +113,10 @@
 	// import EditResumeComponent from '~/components/resume/EditResumeComponent'
 	import { auth } from '~/plugins/firebase-client-init'
 	import Noty from 'noty'
+	import { GChart } from 'vue-google-charts'
+	import moment from 'moment'
 	export default {
+		components: { GChart },
 		layout: 'layoutBack',
 		middleware: [],
 		// components: { CreateResumeComponent, EditResumeComponent },
@@ -113,7 +140,43 @@
 		        ],
 		        snackbar: false,
 		        confirm: false,
-		        resume: {}
+				resume: {},
+				// minDate: moment().subtract(2, 'months'),
+				minDate: new Date(),
+				maxDate: new Date(),
+				chartOptions: {
+					chart: {
+						title: "Clicks",
+						subtitle: "Number of clicks on resume"
+					},
+					width: "100%",
+					height: 400,
+					bar: { groupWidth: "95%" },
+					isStacked: true,
+					colors: ["#7A528F", "#FFC107", "#E11566"],
+					// colors: ["#7A528F"],
+					hAxis: {
+						viewWindowMode: "explicit",
+						viewWindow: {
+							// min: new Date("2019-06-01"),
+							min: this.minDate,
+							// max: new Date("2019-06-31"),
+							max: this.maxDate
+						},
+						// slantedText:false, slantedTextAngle:45
+					},
+					vAxis: {
+						viewWindow: {
+							// max: 10
+							// max: 
+						}
+					},
+					legend: {
+						position: 'top',
+						alignment: 'center'
+					},
+					chartArea: { width: '85%', height: '70%'},
+				}
 			}
 		},
 		computed: {
@@ -122,6 +185,53 @@
 			},
 			loadedUserResumes () {
 				return this.$store.getters['resumes/loadedUserResumes']
+			},
+			getStatistics () {
+				const statistics = []
+				this.loadedUserResumes.forEach(resume => {
+					if (resume.statistics_last_visits) {
+						statistics.push(
+							resume.statistics_last_visits.map(visit => {
+								// return moment(visit * 1000).toDate()
+								// return new Date(moment(visit * 1000).format('YYYY-MM-DD'))
+								// return moment(visit * 1000).format('YYYY-MM-DD')
+								return new Date(moment(visit * 1000).format('YYYY-MM-DD'))
+							})
+						)
+					}
+				})
+				return statistics
+			},
+			chartData () {
+				let newArray = []
+				this.getStatistics.forEach((resume, index) => {
+					console.log('resume: ', resume)
+					
+					resume.forEach((click, index2) => {
+						console.log('click: ', click.getTime())
+						// console.log('filter: ', newArray.filter(value => value[0] === '2019-06-27'))
+						const existingValue = newArray.find(value => value[0].getTime() === click.getTime())
+						console.log('existingValue1: ', existingValue)
+						if (existingValue) {
+							console.log('existing value!')
+							existingValue[index + 1] += 1 || 1
+						} else {
+							const newEntry = new Array(this.getStatistics.length + 1).fill(0)
+							newEntry[0] = click
+							newEntry[index + 1] = 1
+							newArray.push(newEntry)
+							console.log('newEntry: ', newEntry)
+						}
+					})
+				})
+				// newArray.unshift(['Date', 'View #1', 'Views #2'])
+				const headerArray = ['Date']
+				this.loadedUserResumes.forEach(resume => {
+					headerArray.push(`Resume ${resume.slug}`)
+				})
+				newArray.unshift(headerArray)
+				console.log('newArray: ', newArray)
+				return newArray
 			}
 		},
 		methods: {
@@ -149,6 +259,13 @@
 			requestConfirmation (resume) {
 				this.resume = resume
 				this.snackbar = true
+			},
+			onChartReady (chart, google) {
+      			// now we have google lib loaded. Let's create data table based using it.
+      			this.createDataTable(google)
+			},
+			createDataTable(google) {
+				const data = new google.visualization.DataTable();
 			},
 			async deleteResume () {
 				try {
