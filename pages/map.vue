94d8becf64bcdf99bdba8eceb4386efc0da9649e
jeanquark <jm.kleger@gmail.com>
2019-08-07 12:02:19 +0200

    Get center, zoom and marker position variables

--- pages/map.vue
+++ pages/map.vue
@@ -1,44 +1,52 @@
 <template>
-    <div>
-        <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" data-projection="EPSG:4326" style="height: 400px">
-            <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+    <v-app>
+        <h1>Leaflet map</h1>
+		position: {{ position }}<br />
+		zoom: {{ zoom }}<br />
+		center: {{ center }}<br />
+        <div id="map-wrap" style="height: 100vh">
+            <no-ssr>
+                <l-map :zoom.sync="zoom" :center.sync="center">
+                    <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
 
-            <vl-geoloc @update:position="geolocPosition = $event">
-                <template slot-scope="geoloc">
-                    <vl-feature v-if="geoloc.position" id="position-feature">
-                        <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
-                        <vl-style-box>
-                            <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
-                        </vl-style-box>
-                    </vl-feature>
-                </template>
-            </vl-geoloc>
-
-            <vl-layer-tile id="osm">
-                <vl-source-osm></vl-source-osm>
-            </vl-layer-tile>
-        </vl-map>
-        <div style="padding: 20px">
-            Zoom: {{ zoom }}<br>
-            Center: {{ center }}<br>
-            Rotation: {{ rotation }}<br>
-            My geolocation: {{ geolocPosition }}
+                    <l-marker :lat-lng.sync="position" :draggable="true">
+                        <l-popup>Hello <b>Jean-Marc</b>! Lorsdfjsdfhj dfhsjkdfhdsj djfkjs<v-btn small color="success">Click me</v-btn>
+                        </l-popup>
+                    </l-marker>
+                    <!-- <l-circle-marker :lat-lng="[51.509865, -0.118092]" :radius="10" :color="'red'"></l-circle-marker> -->
+                </l-map>
+            </no-ssr>
         </div>
-    </div>
+    </v-app>
 </template>
 
 <script>
-	// import VueLayers from '~/plugins/vue-layers'
-	// import 'vuelayers/lib/style.css' // needs css-loader
+	let L = { icon(){} };
+	if (process.browser) L = require('leaflet');
 	export default {
-		// components: { VueLayers },
 		data() {
 			return {
-				zoom: 2,
-				center: [0, 0],
-				rotation: 0,
-				geolocPosition: undefined
+				// customIcon: this.defaultIcon
+				customIcon: L.icon({
+					iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+					shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+					iconSize: [38, 95],
+					shadowSize: [50, 64],
+					iconAnchor: [22, 94],
+					shadowAnchor: [4, 62],
+					popupAnchor: [-3, -76]
+				}),
+				zoom: 3,
+				center: [51.509865, -0.118092],
+				position: [51.509865, -0.118092]
 			}
 		}
 	}
 </script>
+
+<style>
+	.custom-popup, .leaflet-popup-tip-container {
+    width:3000px;
+    height:15px;
+    }
+</style>
